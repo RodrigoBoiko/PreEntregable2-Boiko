9ad3e5d04e1e1fbab4a80eef2efa7ee35efd1115
RodrigoB <rodrigoeboiko@gmail.com>
2024-08-05 16:27:17 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-        Hello world! my app react 
-        </h1>
-      </div>
-      
+    <Navbar />
     </>
   )
 }
