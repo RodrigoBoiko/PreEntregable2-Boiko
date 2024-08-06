28993a568d0d33a8f164963ed9401fb66cc36154
RodrigoB <rodrigoeboiko@gmail.com>
2024-08-06 14:25:18 -0300

    terminando el hero y el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
-import LOGO from "../assets/logo.png"
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-neutral-700/80">
-        <div className="container px-4 mx-auto relative text-sm">
-            <div className="flex justify center items-center">
-                <div className="flex items-center flex-shrink-0">
-                    <img className= "h-10 w-10 mr-2" src={LOGO} alt="logo" />
-                    <span className="text-xl tracking-tight">RlP</span>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <nav className="bg-white shadow dark:bg-gray-800">
+        
+    <div className="container flex items-center justify-center p-2 mx-auto text-gray-600 capitalize dark:text-gray-300">
+      <img className="h-12 w-12 mr-2" src={logo} alt="logo"/>
+        <a href="#" className="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">home</a>
+
+        <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Ropa masculina</a>
+
+        <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Ropa Femenina</a>
+
+        <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Calzado</a>
+
+    </div>
+</nav>
   )
 }
 
